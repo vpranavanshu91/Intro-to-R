b462cf206acd575d20db7917cd74d385e110af67
vpranavanshu91 <vpranavanshu91@gmail.com>
2015-07-27 10:55:41 +0530

    Comments

--- Functions.r
+++ Functions.r
@@ -7,6 +7,6 @@ nchar(x)
 mean(x,trim = 0.1) #gets rid of 10% of observations on the either side to calculate mean
 mean(x,.1)
 
-x[c(2,5,6)]<-NA
-mean(x)
-mean(x,na.rm = TRUE)
+x[c(2,5,6)]<-NA   #replacing some values of array with NA
+mean(x)         #mean does not return, because array now has NA
+mean(x,na.rm = TRUE)    #use na.rm=TRUE to neglect NA values and return mean for the rest 
