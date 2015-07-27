5ca5259e00454bfb3b94e9bf06ad96971a92e97c
vpranavanshu91 <vpranavanshu91@gmail.com>
2015-07-27 23:36:38 +0530

    comments

---
+++ DataSetsInPackages.r
@@ -0,0 +1,8 @@
+require(ggplot2)
+data(diamonds)      #Diamonds DATA present inside ggplot2 is loaded
+head(diamonds)      #Display Heads of Diamonds table
+
+data("tips",package = "reshape2")   #Tips data presnt in reshape2 package loaded without the package being loaded
+head(tips)
+
+data()    #displays all the data's available in a separate tab
