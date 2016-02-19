86e81a770aabe9f686689aa1f347fdd7467ef04a
Pranav <vpranavanshu91@gmail.com>
2016-02-19 17:37:39 +0530

    Update Aggregation.r

--- Aggregation.r
+++ Aggregation.r
@@ -9,5 +9,5 @@ aggregate(price~cut,diamonds,mean) #Aggregates Price and segment factor is cut
 aggregate(price~cut,diamonds,mean, na.rm=TRUE)
 
 aggregate(price~cut+color,data = diamonds,FUN = mean)  #Aggregates Price with unique combo of cut and color
-aggregate(cbind(price,carat)~cut,data = diamonds,FUN = mean) #Aggeegates Price and Carat with unique combo of cut and color
-aggregate(cbind(price,carat)~cut+color,data = diamonds,FUN = mean)
+aggregate(cbind(price,carat)~cut,data = diamonds,FUN = mean) 
+aggregate(cbind(price,carat)~cut+color,data = diamonds,FUN = mean) #Aggeegates Price and Carat with unique combo of cut and color
