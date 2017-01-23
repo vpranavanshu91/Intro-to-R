b3f04de7d9dc5f895bac2a91651278e8fd071082
vpranavanshu91 <vpranavanshu91@gmail.com>
2017-01-23 11:02:02 -0500

    done

---
+++ Links_for_project.R
@@ -0,0 +1,4 @@
+https://flovv.shinyapps.io/image-shiny/
+https://www.r-bloggers.com/image-recognition-and-face-detection/
+https://www.r-bloggers.com/analyze-face-emotions-with-r/
+https://cran.r-project.org/web/packages/magick/vignettes/intro.html
