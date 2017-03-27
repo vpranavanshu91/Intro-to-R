b81d15cb9ab339da6e6b1bbddb3103547c5b8bdd
Pranavanshu V <Pranavanshu V>
2017-03-27 00:01:00 -0400

    comments

--- runbatch.r
+++ runbatch.r
@@ -1,1 +1,7 @@
 shell('C: & cd C:/Users/vpran/Desktop & batch.bat')
+
+cd "C:\Users\vpran\Documents\GitHub\Intro-to-R"
+git.exe pull
+git.exe add .
+git.exe commit -m "comments"
+git.exe push origin master
