54b4a424c52dc96fc4cdfb2039f0d89f11cc6fd9
vpranavanshu91 <vpranavanshu91@gmail.com>
2017-03-26 23:56:00 -0400

    comments

--- Neeraj.r
+++ Neeraj.r
@@ -3,3 +3,15 @@
 #hi yo ssup
 #hey yo pranav here 123
 #RStudio
+
+cd C:/Users/vpran/Documents/GitHub/Intro-to-R
+
+#to pull
+git pull
+
+#to stage
+git add .
+#to commit
+git commit -m "comments"
+#to push
+git push origin master
