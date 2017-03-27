300af3ee098319b3fe9c19a52deabfa1197c42a6
Pranavanshu V <Pranavanshu V>
2017-03-27 15:25:16 -0400

    comments

--- Addin_test.R
+++ Addin_test.R
@@ -1,6 +1,7 @@
-require(rstudioapi)
-sendToConsole(code, execute = TRUE)
+commit = function()
+{
+    code = shell('C: & cd C:/Users/vpran/Desktop & cd "C:/Users/vpran/Documents/GitHub/Intro-to-R" & git.exe pull & git.exe add . & git.exe commit -m "comments" & git.exe push origin master')
+    cat("\014")
+    
+}
 
-
-shell('C: & cd C:/Users/vpran/Desktop & cd "C:\Users\vpran\Documents\GitHub\Intro-to-R" & git.exe pull & git.exe add . & git.exe commit -m "comments" & git.exe push origin master')
-cat("\014")
