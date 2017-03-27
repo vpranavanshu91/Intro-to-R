4cfe8c9d788facff487d030e2fcbfb892955ee3e
Pranavanshu V <Pranavanshu V>
2017-03-27 15:13:08 -0400

    comments

---
+++ Addin_test.R
@@ -0,0 +1,6 @@
+require(rstudioapi)
+sendToConsole(code, execute = TRUE)
+
+
+shell('C: & cd C:/Users/vpran/Desktop & cd "C:\Users\vpran\Documents\GitHub\Intro-to-R" & git.exe pull & git.exe add . & git.exe commit -m "comments" & git.exe push origin master')
+cat("\014")
