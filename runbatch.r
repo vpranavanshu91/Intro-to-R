1005207ce6c9e06b35e941c797b29334a452f718
Neeraj Subhedar <Neeraj Subhedar>
2017-03-27 00:05:54 -0400

    comments

--- runbatch.r
+++ runbatch.r
@@ -1,10 +1,2 @@
-shell('C: & cd C:/Users/vpran/Desktop & batch.bat')
-
-cd "C:\Users\vpran\Documents\GitHub\Intro-to-R"
-git.exe pull
-git.exe add .
-git.exe commit -m "comments"
-git.exe push origin master
-
-
-#fnjb
+shell('C: & cd C:/Users/NeerajSubhedar/Desktop & batch.bat')
+cat("\014")
