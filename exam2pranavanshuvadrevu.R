0bbb14d38fa5e7356cbb1212f441784e36a88465
Pranavanshu V <Pranavanshu V>
2017-04-17 09:55:29 -0400

    commit

--- exam2pranavanshuvadrevu.R
+++ exam2pranavanshuvadrevu.R
@@ -23,23 +23,37 @@ summary(res)
 
 ##################################################################################
 
-wrkh <- read.csv("C:/Users/vpran/Desktop/Workinghours.csv")
-attach(wrkh)
+wrkh <- read.csv("C:/Users/vpran/Desktop/Workinghours.csv")  #reading work hours file
+attach(wrkh)  #attaching work hours file
 
 plot(income)
 
-reg <- lm(income~.-child5-child13-child17,data=wrkh)
-summary(reg)
+reg <- lm(income~.-child5-child13-child17,data=wrkh)   #running first regression without child 5, 13 and 17
+summary(reg)  #viewing summary
 
-boxCox(reg,family = "yjPower", plotit = T)
+boxCox(reg,family = "yjPower", plotit = T)  #using boxcox to ecaluate the right transformation
 
-reg2 <- lm(log(income)~.-child5-child13-child17,data=wrkh)
+reg2 <- lm(log(income)~.-child5-child13-child17,data=wrkh)   #since the boxcox lambda is between 0 and 0.5 , its close to 0 hence considering log
 summary(reg2)
 
-z = cooks.distance(reg2)
-cutoff = 4 / length(income)
+z = cooks.distance(reg2)   #calculating z using cooks distance
+cutoff = 4 / length(income)    #calculating cutoff value
 
-plot(reg2,which = 4, cook.levels = cutoff)
+plot(reg2,which = 4, cook.levels = cutoff)   #plotting the cooks distance and the cutoff line
 abline(h = cutoff,col='red')
-z[z>cutoff]
-length(z[z>cutoff])
+
+z[z>cutoff]    #all influential observations above the cutoffline
+length(z[z>cutoff])   #the no, of influential observations
+
+
+#check lambda value peak and decide what transformation to apply
+#
+#Table 1: Common Box-Cox Transformations
+#  l 	    Y'
+# -2  	  Y-2 = 1/Y2
+# -1	    Y-1 = 1/Y1
+# -0.5  	Y-0.5 = 1/(Sqrt(Y))
+#  0	    log(Y)
+#  0.5	  Y0.5 = Sqrt(Y)
+#  1	    Y1 = Y
+#  2	    Y2
