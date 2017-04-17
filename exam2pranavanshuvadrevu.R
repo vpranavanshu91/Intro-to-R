7c337a9c13559d247311ceb6780a26cadd674a38
Pranavanshu V <Pranavanshu V>
2017-04-17 09:51:13 -0400

    commit

--- exam2pranavanshuvadrevu.R
+++ exam2pranavanshuvadrevu.R
@@ -1,25 +1,25 @@
-library(bbmle)
-library(car)
+library(bbmle)   #Loading bbmle package
+library(car)     #loading car package
 
-growth <- read.csv("C:/Users/vpran/Desktop/growth.csv")
+growth <- read.csv("C:/Users/vpran/Desktop/growth.csv")    #reading growth file
 
-attach(growth)
+attach(growth)  #attaching growth
 
-plot(growth$age,growth$height)
+plot(growth$age,growth$height)   #plotting age and height
 
-f1 = function(b0,b1,sig)
+f1 = function(b0,b1,sig)    # writing a function that takes b0,b1,sig as input
 {
-    err1 = height - b0 - b1*age
-    err2 = height - b0 - b1*20
-    L = ifelse(age<=15,dnorm(err1,mean = 0,sd=sig),dnorm(err2,mean = 0,sd=sig))
-    LLsum = sum(log(L))
-    return(-1*LLsum)
+    err1 = height - b0 - b1*age             #defining err for when age is less than equal to 15
+    err2 = height - b0 - b1*20             #defining err for when age is above 15
+    L = ifelse(age<=20,dnorm(err1,mean = 0,sd=sig),dnorm(err2,mean = 0,sd=sig))     
+    LLsum = sum(log(L))    #sum of log of probabilities
+    return(-1*LLsum)    #returns -llsum to minimize
 }
 
 f1(0,0,sd(height))
-res = mle2(minuslogl = f1, start = list(b0=mean(height), b1=0, sig=sd(height)))
+res = mle2(minuslogl = f1, start = list(b0=mean(height), b1=0, sig=sd(height)))  #giving baseline model starting values
 summary(res)  
-#"L-BFGS-B",lower = c(sig=0)
+
 
 ##################################################################################
 
@@ -37,8 +37,9 @@ reg2 <- lm(log(income)~.-child5-child13-child17,data=wrkh)
 summary(reg2)
 
 z = cooks.distance(reg2)
+cutoff = 4 / length(income)
+
 plot(reg2,which = 4, cook.levels = cutoff)
 abline(h = cutoff,col='red')
-cutoff = 4 / length(income)
 z[z>cutoff]
 length(z[z>cutoff])
